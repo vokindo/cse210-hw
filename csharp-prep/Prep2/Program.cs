00660eab3a1590771748ad1ca1619d40849a47ba
vokindo <okindovictor7@gmail.com>
2024-09-25 18:14:48 +0300

    Stretch challenge completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -9,8 +9,9 @@ class Program
         Console.Write("Enter your grade percentage: ");
         int gradePercentage = int.Parse(Console.ReadLine());
 
-        // Declare a variable to store the letter grade
+        // Declare a variable to store the letter grade and now sign
         string letter = "";
+        string sign = "";
 
         // Determine the letter grade based on the percentage
         if (gradePercentage >= 90)
@@ -35,7 +36,45 @@ class Program
         }
 
         // Print the letter grade
-        Console.WriteLine($"Your letter grade is: {letter}");
+        //Console.WriteLine($"Your letter grade is: {letter}");
+
+        // Determine if the user passed the course
+        //if (gradePercentage >= 70)
+        {
+          //  Console.WriteLine("Congratulations, you passed the course!");
+        }
+        //else
+        {
+          //  Console.WriteLine("Unfortunately, you did not pass. Better luck next time!");
+        }
+
+        // Determine the sign if the grade is not "F" or "A"
+        if (letter != "F")
+        {
+            int lastDigit = gradePercentage % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+         // Handle special case for "A+" and "F+/F-"
+        if (letter == "A" && sign == "+")
+        {
+            sign = ""; // No "A+"
+        }
+        if (letter == "F")
+        {
+            sign = ""; // No "F+" or "F-"
+        }
+
+        // Print the letter grade with the sign
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
 
         // Determine if the user passed the course
         if (gradePercentage >= 70)
@@ -46,7 +85,8 @@ class Program
         {
             Console.WriteLine("Unfortunately, you did not pass. Better luck next time!");
         }
-
     }
+
+    
     
 }
