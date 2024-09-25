5f3e8033139c5e18a74c624b54159e90e61f04c1
vokindo <okindovictor7@gmail.com>
2024-09-25 15:30:35 +0300

    Prep 1 assignment done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompt the user for their first name
+
+        Console.Write("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        // Prompt the user for their last name
+
+        Console.Write("What is your last name?");        
+        string last_name = Console.ReadLine();
+
+        //Display Results
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
