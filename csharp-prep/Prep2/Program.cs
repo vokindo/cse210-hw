5270ba5071973598c31a325f1314e3da25dbaad1
vokindo <okindovictor7@gmail.com>
2024-09-25 17:24:08 +0300

    Q! !Q

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,50 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {//This is Prep2
-        Console.WriteLine("Hello Prep2 World!");
+    
+    {//ask the user for their grade percentage
+
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        // Declare a variable to store the letter grade
+        string letter = "";
+
+        // Determine the letter grade based on the percentage
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Print the letter grade
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        // Determine if the user passed the course
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Better luck next time!");
+        }
+
     }
+    
 }
